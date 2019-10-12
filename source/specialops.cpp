a2c495616bcebe976a47d7f49f5cbcacb591a779
konstanteen <kostmetallist@gmail.com>
2019-10-12 18:36:08 +0300

    ellpack matrix & plain matrix struct definitions; plain to ellpack conversion function added

--- source/specialops.cpp
+++ source/specialops.cpp
@@ -1,7 +1,7 @@
 #include "specialops.h"
 #include <vector>
 #include <iostream>
-#include <stdio.h>
+#include <cmath>
 
 namespace so {
 
@@ -35,5 +35,58 @@ namespace so {
         }
 
         return dot_result;
+    }
+
+    ellpack_matrix plain2ellpack(plain_matrix &matrix, 
+        std::size_t max_nonzero) {
+
+        std::size_t rows_number = matrix.rows.size();
+        std::vector<std::vector<int>> idxs(rows_number);
+        std::vector<std::vector<double>> data(rows_number);
+
+        for (std::size_t i = 0; i < rows_number; ++i) {
+
+            idxs[i] = std::vector<int>(max_nonzero);
+            data[i] = std::vector<double>(max_nonzero);
+
+            std::vector<double> origin_row = matrix.rows[i];
+            std::vector<int> idxs_row = idxs[i];
+            std::vector<double> data_row = data[i];
+            // index for iterating over idxs and data
+            std::size_t k = 0;
+            for (std::size_t j = 0; 
+                 k < max_nonzero && j < origin_row.size(); 
+                 ++j) {
+
+                double value = origin_row[j];
+                // checking whether the value is nonzero
+                if (abs(value) > EPS) {
+                    idxs_row[k] = j;
+                    data_row[k] = value;
+                    k++;
+                }
+            }
+
+            // if all the elements were filled, ok;
+            // otherwise, create the padding containing 
+            // last consistent value in the vector
+            if (k != max_nonzero) {
+
+                int tail_idxs_elem = idxs_row[k-1];
+                int tail_data_elem = data_row[k-1];
+                while (k < max_nonzero) {
+
+                    // padding 
+                    idxs_row[k] = tail_idxs_elem;
+                    data_row[k] = tail_data_elem;
+                    k++;
+                }
+            }
+        }
+
+        ellpack_matrix result;
+        result.idxs = idxs;
+        result.data = data;
+        return result;
     }
 }
--- source/specialops.h
+++ source/specialops.h
@@ -2,15 +2,24 @@
 #define SPECIALOPS_H
 #include <vector>
 
+#define EPS 1E-6
+
 typedef struct {
-    /* data */
-} sparse_matrix;
+    std::vector<std::vector<double>> rows;
+} plain_matrix;
+
+typedef struct 
+{
+    std::vector<std::vector<int>> idxs;
+    std::vector<std::vector<double>> data;
+} ellpack_matrix;
 
 namespace so {
 
     std::vector<double> axpby(std::vector<double> &x, double a, 
         std::vector<double> &y, double b);
     double dot(std::vector<double> &x, std::vector<double> &y);
+    ellpack_matrix plain2ellpack(plain_matrix &matrix, std::size_t max_nonzero);
 }
 
 #endif
